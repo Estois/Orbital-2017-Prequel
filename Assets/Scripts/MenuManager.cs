251f85e92ee45dc8dd68eb0a1a9161b28ff2f1c9
Polar <yan.wc@hotmail.com>
2017-08-20 12:05:17 +0800

    20 August

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -12,7 +12,7 @@ public class MenuManager : MonoBehaviour
 	public void ExitGame ()
 	{
 		Debug.Log ("QUIT");
-		UnityEditor.EditorApplication.isPlaying = false;
+//		UnityEditor.EditorApplication.isPlaying = false;
 		Application.Quit ();
 	}
 
